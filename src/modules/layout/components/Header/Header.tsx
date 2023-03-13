051fdd053c05042e3f3250c89e0509fdc71d466f
Sergey P <s.pansevi4@gmail.com>
2023-03-13 15:37:29 +0300

    feat: add styles for Header component

--- src/modules/layout/components/Header/Header.tsx
+++ src/modules/layout/components/Header/Header.tsx
@@ -1,10 +1,22 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import cls from './Header.module.scss';
+
+interface ActiveObjectLink {
+  isActive: boolean;
+  isPending: boolean;
+}
 
 export default function Header() {
+  const isActiveLink = ({ isActive }: ActiveObjectLink) => (isActive ? cls['active-link'] : '');
+
   return (
     <header>
-      <Link to="/home">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink to="/home" className={isActiveLink}>
+        Home
+      </NavLink>
+      <NavLink to="/about" className={isActiveLink}>
+        About
+      </NavLink>
     </header>
   );
 }
