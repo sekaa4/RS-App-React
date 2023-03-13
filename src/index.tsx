d2605015924483f4d47f3d162434983a07749b9b
Sergey P <s.pansevi4@gmail.com>
2023-03-13 13:40:20 +0300

    refactor: change directory for App.tsx file and rename main.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import { WrappedApp } from 'App/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { WrappedApp } from './App';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
