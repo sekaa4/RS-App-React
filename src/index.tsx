2e8ddf123c837af53f4a3bc5cee7a1777e149ca7
Sergey P <s.pansevi4@gmail.com>
2023-03-13 17:56:59 +0300

    feat: add main styles for the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import { WrappedApp } from 'App/App';
+import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import { WrappedApp } from 'App/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
