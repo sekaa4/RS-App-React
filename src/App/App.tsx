900a392d22a894d381ea0566b3fd8e3780cab019
Sergey P <s.pansevi4@gmail.com>
2023-03-13 13:48:34 +0300

    fix: change App.tsx file for work with Outlet component

---
+++ src/App/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Home, About, NotFound } from 'pages';
+import Layout from 'modules/layout';
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
