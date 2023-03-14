0c69bd064a07cb37d7185e5937485ae39ea9122e
Sergey P <s.pansevi4@gmail.com>
2023-03-14 19:17:27 +0300

    fix: change Button component

--- src/modules/common/components/Button/Button.tsx
+++ src/modules/common/components/Button/Button.tsx
@@ -1,13 +1,24 @@
 /* eslint-disable class-methods-use-this */
 import { Component } from 'react';
 
-export default class Button extends Component {
-  onClickHandler() {}
+interface ButtonProps {
+  handleClick?: () => void;
+}
+
+export default class Button extends Component<ButtonProps> {
+  handleClick?: () => void;
+
+  constructor(props: ButtonProps) {
+    super(props);
+
+    const { handleClick } = this.props;
+    this.handleClick = handleClick?.bind(this);
+  }
 
   render() {
     return (
       <div>
-        <button type="button" onClick={this.onClickHandler}>
+        <button type="button" onClick={this.handleClick}>
           Click
         </button>
       </div>
