3de91385194c3bc1e5f171596a12f5ab42b4e1ea
Sergey P <s.pansevi4@gmail.com>
2023-03-14 18:32:34 +0300

    fix: change work Input component with localStorage

--- src/modules/common/components/Input/Input.tsx
+++ src/modules/common/components/Input/Input.tsx
@@ -1,47 +1,53 @@
 /* eslint-disable react/sort-comp */
+import Constants from 'models/Constants';
 import { ChangeEvent, Component } from 'react';
 
-class Input extends Component {
+interface InitialState {
+  value: string;
+}
+export default class Input extends Component {
   constructor(props = {}) {
     super(props);
     this.state = {
-      value: '',
+      value: localStorage.getItem(Constants.SEARCH_KEY) ?? '',
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(e: ChangeEvent) {
+  handleChange(e: ChangeEvent<HTMLInputElement>) {
     const { value } = e.target;
-    this.setState((prevState) => {
+    this.setState(() => {
       return {
-        ...prevState,
         value,
       };
     });
   }
 
-  componentDidMount() {
-    localStorage.setItem('key', 'didMount');
-  }
+  componentDidMount() {}
 
   componentDidUpdate() {}
 
   componentWillUnmount() {
-    localStorage.setItem('key', 'willUnmount');
+    const { value } = this.state as InitialState;
+    localStorage.setItem('key', value);
   }
 
   render() {
-    const { value } = this.state;
+    const { value } = this.state as InitialState;
+
     return (
       <div>
         <label htmlFor="search-bar">
-          <input type="text" onChange={this.handleChange} value={value} id="search-bar" />
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={value}
+            id="search-bar"
+            placeholder="enter value here"
+          />
         </label>
-        {value}
       </div>
     );
   }
 }
-
-export default Input;
