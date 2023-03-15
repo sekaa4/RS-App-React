39838b2d6f0722f56abbec4592d0fe4b5bd4f32b
Sergey P <s.pansevi4@gmail.com>
2023-03-15 21:27:34 +0300

    fix: change baseURL in vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/RS-App-React/',
   test: {
     globals: true,
     environment: 'jsdom',
