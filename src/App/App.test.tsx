b865f9e49435bdd4daaf973d5d92adfc30009059
Sergey P <s.pansevi4@gmail.com>
2023-03-19 15:38:58 +0300

    feat: add tests for App component

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,30 +1,52 @@
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
+import Data from 'models/Data.type';
 import App from './App';
 import WrappedApp from './WrappedApp';
 
+const data: Data[] = [
+  {
+    userId: 1,
+    id: 1,
+    name: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    birthDate: '2018-03-09',
+    age: 5,
+    gender: 'male',
+    breeds: 'Aegean',
+    img: 'https://cdn2.thecatapi.com/images/ehc.jpg',
+  },
+  {
+    userId: 2,
+    id: 2,
+    name: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    birthDate: '2018-03-09',
+    age: 5,
+    gender: 'male',
+    breeds: 'Aegean',
+    img: 'https://cdn2.thecatapi.com/images/ehc.jpg',
+  },
+];
+
+global.fetch = vi.fn().mockResolvedValueOnce({
+  json: () => Promise.resolve(data),
+});
+
 describe('App', () => {
   it('Renders <Home Page>', () => {
-    // arrange
     render(<WrappedApp />);
-
-    // act
-
-    // expect
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Home Page');
   });
 
   it('Renders <Not Found> if invalid path', () => {
-    // arrange
     render(
       <MemoryRouter initialEntries={["/this-route-doesn't-exist"]}>
         <App />
       </MemoryRouter>
     );
-
-    // expect
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Not Found Page');
   });
 });
