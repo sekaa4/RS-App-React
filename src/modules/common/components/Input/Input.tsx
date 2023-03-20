82b008ff11c5eff59d1b44cd7ac8254ef635def7
Sergey P <s.pansevi4@gmail.com>
2023-03-20 14:30:03 +0300

    feat: add InputSearch component as container for Input component

--- src/modules/common/components/Input/Input.tsx
+++ src/modules/common/components/Input/Input.tsx
@@ -1,65 +1,36 @@
-import Constants from 'models/Constants';
-import { ChangeEvent, Component } from 'react';
+import { ChangeEvent, PureComponent } from 'react';
 import cls from './Input.module.scss';
-
-interface InputState {
-  value: string;
-}
 
 interface InputProps {
   classNames?: string[];
+  type: string;
+  value?: string;
+  id: string;
+  placeholder?: string;
+  inputStyles?: string[];
+  handleChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
-export default class Input extends Component<InputProps, InputState> {
-  constructor(props: InputProps) {
-    super(props);
-    this.state = {
-      value: localStorage.getItem(Constants.SEARCH_KEY) ?? '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.onUnload = this.onUnload.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener(Constants.BEFOREUNLOAD, this.onUnload);
-  }
-
-  componentWillUnmount() {
-    const { value } = this.state;
-    localStorage.setItem(Constants.SEARCH_KEY, value);
-
-    window.removeEventListener(Constants.BEFOREUNLOAD, this.onUnload);
-  }
-
-  handleChange(event: ChangeEvent<HTMLInputElement>) {
-    const {
-      target: { value },
-    } = event;
-    this.setState(() => {
-      return {
-        value,
-      };
-    });
-  }
-
-  onUnload() {
-    const { value } = this.state;
-    localStorage.setItem(Constants.SEARCH_KEY, value);
-  }
-
+export default class Input extends PureComponent<InputProps> {
   render() {
-    const { value } = this.state;
-    const { classNames = '' } = this.props;
+    const {
+      classNames = '',
+      type = 'text',
+      value = '',
+      id,
+      placeholder = '',
+      inputStyles = [],
+      handleChange,
+    } = this.props;
 
     return (
-      <label htmlFor="search-bar" className={[...classNames].join(' ')}>
+      <label htmlFor={id} className={[...classNames].join(' ')}>
         <input
-          type="text"
-          onChange={this.handleChange}
+          type={type}
+          onChange={handleChange}
           value={value}
-          id="search-bar"
-          placeholder="Search bar..."
-          className={cls['input-search']}
+          id={id}
+          placeholder={placeholder}
+          className={[cls.input, ...inputStyles].join(' ')}
         />
       </label>
     );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,16 @@
 import { PureComponent } from 'react';
 import CardList from 'modules/card-list/components/CardList/CardList';
-import { Button, Input } from 'modules/common';
+import InputSearch from 'modules/input-search/components/InputSearch/InputSearch';
+import { Button } from 'modules/common';
 import cls from './Home.module.scss';
 
 export default class Home extends PureComponent {
   render() {
-    const styles = [cls['search-bar']];
     return (
       <div className={cls.home}>
         <h2>Home Page</h2>
         <div className={cls['home__search-bar']}>
-          <Input classNames={styles} />
+          <InputSearch />
           <Button />
         </div>
         <CardList />
