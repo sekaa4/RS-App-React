bdf4d812f3b401bd88aff58bb36ccf59e0150500
Sergey P <s.pansevi4@gmail.com>
2023-03-21 20:44:32 +0300

    feat: add FormContainer component to the From page component

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -1,12 +1,14 @@
+import FormContainer from 'modules/form-container/components/FormContainer/FormContainer';
 import { PureComponent } from 'react';
 import cls from './Form.module.scss';
 
 export default class Form extends PureComponent {
   render() {
     return (
-      <div className={cls.form}>
+      <section className={cls.form}>
         <h2>Form Page</h2>
-      </div>
+        <FormContainer />
+      </section>
     );
   }
 }
