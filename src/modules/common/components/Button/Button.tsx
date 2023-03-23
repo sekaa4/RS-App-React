f8d5e9709fdf0ddaf01f8c8ec2de7a2ecebd7d03
Sergey P <s.pansevi4@gmail.com>
2023-03-23 20:31:07 +0300

    feat: add possibility for Button component as submit button for form

--- src/modules/common/components/Button/Button.tsx
+++ src/modules/common/components/Button/Button.tsx
@@ -1,21 +1,36 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import cls from './Button.module.scss';
 
 interface ButtonProps {
   handleClick?: () => void;
+  refSubmit?: React.RefObject<HTMLInputElement | HTMLButtonElement>;
 }
 
 export default class Button extends Component<ButtonProps> {
   handleClick?: () => void;
 
+  refSubmit?: React.RefObject<HTMLButtonElement>;
+
   constructor(props: ButtonProps) {
     super(props);
 
-    const { handleClick } = this.props;
-    this.handleClick = handleClick?.bind(this);
+    const { handleClick, refSubmit } = this.props;
+
+    if (refSubmit) {
+      this.refSubmit = refSubmit as React.RefObject<HTMLButtonElement>;
+    } else {
+      this.handleClick = handleClick?.bind(this);
+    }
   }
 
   render() {
+    if (this.refSubmit) {
+      return (
+        <button type="submit" className={cls.button} ref={this.refSubmit}>
+          Submit
+        </button>
+      );
+    }
     return (
       <button type="button" onClick={this.handleClick} className={cls.button}>
         search
