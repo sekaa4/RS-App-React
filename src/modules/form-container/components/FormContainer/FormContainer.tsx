9bf0040029314849139e71fda6259b375b20ce2b
Sergey P <s.pansevi4@gmail.com>
2023-03-26 12:20:21 +0300

    fix: change FormContainer component for works with Uncontrolled components

--- src/modules/form-container/components/FormContainer/FormContainer.tsx
+++ src/modules/form-container/components/FormContainer/FormContainer.tsx
@@ -1,105 +1,117 @@
 import Data from 'models/Data.type';
 import CardList from 'modules/card-list/components/CardList/CardList';
-import React, { Component } from 'react';
-import { Form, UncontrolledInput } from 'modules/common/index';
+import React, { Component, createRef } from 'react';
+import checkValidValue from 'utils/checkValidValue';
+import {
+  Button,
+  Form,
+  UncontrolledInput,
+  UncontrolledRadioInput,
+  UncontrolledSelect,
+  UncontrolledTextArea,
+} from 'modules/common/index';
 import CustomRefObject from 'models/CustomRefObject.type';
+import breeds from 'models/breeds';
+import Constants from 'models/Constants';
 import cls from './FormContainer.module.scss';
+import RadioContainer from '../RadioContainer/RadioContainer';
 
 interface FormContainerState {
   dataCard: Data[] | null;
+  errorObject: Record<keyof CustomRefObject, false | string>;
 }
 
-const data: Data[] = [
-  {
-    userId: 1,
-    id: 1,
-    name: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-    body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
-    birthDate: '2018-03-09',
-    age: 5,
-    gender: 'male',
-    breeds: 'Aegean',
-    img: 'https://cdn2.thecatapi.com/images/ehc.jpg',
-  },
-  {
-    userId: 2,
-    id: 2,
-    name: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-    body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
-    birthDate: '2018-03-09',
-    age: 5,
-    gender: 'male',
-    breeds: 'Aegean',
-    img: 'https://cdn2.thecatapi.com/images/ehc.jpg',
-  },
-];
-
 export default class FormContainer extends Component<Record<string, unknown>, FormContainerState> {
-  formContainer: CustomRefObject;
+  refObject: CustomRefObject;
+
+  refInputRadioObject: React.RefObject<HTMLInputElement>[];
+
+  refSubmit: React.RefObject<HTMLInputElement | HTMLButtonElement>;
+
+  refForm: React.RefObject<HTMLFormElement>;
+
+  errorObject: Record<keyof CustomRefObject, false | string>;
+
+  isValidate: boolean;
+
+  description: string;
+
+  countOfCards: number;
 
   constructor(props: Record<string, unknown>) {
     super(props);
     this.state = {
-      dataCard: data,
+      dataCard: null,
+      errorObject: {},
     };
-    this.formContainer = {};
+
+    const { errorObject } = this.state;
+    this.refObject = {};
+    this.refInputRadioObject = [];
+    this.countOfCards = 0;
+    this.errorObject = errorObject;
+    this.isValidate = true;
+    this.description = Constants.DESCRIPTION_FORM;
+
+    this.refForm = createRef<HTMLFormElement>();
+    this.refSubmit = createRef<HTMLInputElement>();
     this.createCardFromForm = this.createCardFromForm.bind(this);
     this.submitHandler = this.submitHandler.bind(this);
   }
-
-  componentDidMount(): void {}
 
   createCardFromForm(dataObj: Data) {
     this.setState((oldState) => {
       const { dataCard } = oldState;
       if (dataCard) {
-        dataCard.push(dataObj);
-        return { dataCard };
+        return { dataCard: [...dataCard, dataObj] };
       }
-      const newArrData = [dataObj];
-      return { dataCard: newArrData };
+      return { dataCard: [dataObj] };
     });
   }
 
   submitHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const isValidate = this.validate();
-    if (isValidate) {
+    this.validate();
+    if (this.isValidate) {
       const dataObj = this.createDataObject();
-      // this.createCardFromForm(dataObj);
+      this.createCardFromForm(dataObj);
       this.formReset();
 
-      alert('Data Save and create card');
+      setTimeout(() => alert('Data Save and create card'), 500);
+    } else {
+      this.setState({ errorObject: this.errorObject });
     }
   }
 
   createDataObject(): Data {
-    const keys = Object.keys(this.formContainer);
+    this.countOfCards += 1;
+    const keys = Object.keys(this.refObject);
     const dataObj = keys.reduce((acc, field) => {
-      const { current } = this.formContainer[field];
-      if (field === 'form' || !current) return acc;
-      return { ...acc, [field]: current.value };
+      const { current } = this.refObject[field];
+      if (current instanceof HTMLInputElement && current.type === 'file') {
+        const fileList = current.files as FileList;
+        const fileUrl = window.URL.createObjectURL(fileList[0]);
+        return { ...acc, [field]: fileUrl };
+      }
+      return current ? { ...acc, [field]: current.value } : acc;
     }, {} as Data);
 
+    dataObj.id = this.countOfCards;
     return dataObj;
   }
 
-  validate(): boolean {
-    const isValidate = Object.keys(this.formContainer).every((field) => {
-      if (field === 'form' || field === 'submit') return true;
-      const { current } = this.formContainer[field];
-      if (current && current.value === '') {
-        return false;
-      }
-      return true;
-    });
+  validate(): void {
+    const validateArr = [this.refObject, this.refInputRadioObject];
+    const checkValidObj = checkValidValue(validateArr, this.refObject, this.errorObject);
+    this.refObject = checkValidObj.refObject;
+    this.errorObject = checkValidObj.errorObject;
 
-    return isValidate;
+    this.isValidate = Object.values(this.errorObject).every((value) => !value);
   }
 
   formReset() {
-    const { form } = this.formContainer;
-    if (form.current && form.current instanceof HTMLFormElement) form.current.reset();
+    const { current } = this.refForm;
+    if (current) current.reset();
   }
 
   render() {
@@ -107,69 +119,93 @@ export default class FormContainer extends Component<Record<string, unknown>, Fo
 
     return (
       <div className={cls['form-container']}>
-        <Form refer={this.formContainer} submitHandler={this.submitHandler}>
-          <UncontrolledInput
-            type="text"
-            id="name"
-            refer={this.formContainer}
-            placeholder="Enter the name of your cat"
-          />
-          {/* <UncontrolledInput
-            type="textarea"
-            id="description"
-            refer={this.formContainer}
-            defaultValue="Enter description about your cat"
-          /> */}
-          <UncontrolledInput
-            type="date"
-            id="date"
-            refer={this.formContainer}
-            text="Choose the birthday date of your cat"
-          />
-          <UncontrolledInput
-            type="number"
-            id="age"
-            refer={this.formContainer}
-            text="Enter the age of your cat"
-          />
-          {/* <UncontrolledInput
-            type="checkbox"
-            refer={this.formContainer}
-            id="breeds"
-            text="Choose breeds of your cat"
-          /> */}
-          {/* <UncontrolledInput
-            type="checkbox"
-            id="personal-data"
-            refer={this.formContainer}
-            text="I consent to my personal data"
-          /> */}
-          {/* <UncontrolledInput
-            type="radio"
-            id="gender-male"
-            refer={this.formContainer}
-            name="gender"
-            defaultValue="male"
-            text="Male"
-          />
-          <UncontrolledInput
-            type="radio"
-            id="gender-female"
-            refer={this.formContainer}
-            name="gender"
-            defaultValue="female"
-            text="Female"
-          /> */}
-          {/* <UncontrolledInput type="file" id="image" refer={this.formContainer} /> */}
-          <UncontrolledInput
-            type="submit"
-            id="submit"
-            refer={this.formContainer}
-            inputStyles={[cls['submit-button']]}
-            defaultValue="Submit"
-          />
+        <Form
+          refForm={this.refForm}
+          submitHandler={this.submitHandler}
+          description={this.description}
+        >
+          <div>
+            <UncontrolledInput
+              type="text"
+              id="name"
+              text="Enter the name of your cat:"
+              refObject={this.refObject}
+              placeholder="Name of your cat"
+              errorObject={this.errorObject}
+            />
+            <UncontrolledInput
+              type="date"
+              id="birthDate"
+              refObject={this.refObject}
+              errorObject={this.errorObject}
+              text="Choose the birthday date of your cat:"
+            />
+            <UncontrolledInput
+              type="number"
+              id="age"
+              placeholder="Age of your cat"
+              refObject={this.refObject}
+              errorObject={this.errorObject}
+              text="Enter the age of your cat:"
+            />
+            <RadioContainer errorObject={this.errorObject} name="gender">
+              <UncontrolledRadioInput
+                id="gender-male"
+                defaultValue="male"
+                refObject={this.refInputRadioObject}
+                errorObject={this.errorObject}
+                name="gender"
+              />
+              <UncontrolledRadioInput
+                id="gender-female"
+                defaultValue="female"
+                refObject={this.refInputRadioObject}
+                errorObject={this.errorObject}
+                name="gender"
+              />
+            </RadioContainer>
+          </div>
+          <div>
+            <UncontrolledTextArea
+              id="body"
+              name="textareaForm"
+              refObject={this.refObject}
+              errorObject={this.errorObject}
+              text="Enter description of your cat:"
+              placeholder="Enter description about your cat"
+            />
+            <UncontrolledSelect
+              refObject={this.refObject}
+              errorObject={this.errorObject}
+              id="breeds"
+              name="select-breeds"
+              text="Choose breeds of your cat: "
+              options={breeds}
+            />
+            <UncontrolledInput
+              type="file"
+              id="img"
+              text="Choose your cat image:"
+              refObject={this.refObject}
+              errorObject={this.errorObject}
+            />
+            <UncontrolledInput
+              type="checkbox"
+              id="personalData"
+              refObject={this.refObject}
+              errorObject={this.errorObject}
+              text="I consent to my personal data"
+            />
+            <Button refSubmit={this.refSubmit} text="Create Card" />
+          </div>
         </Form>
-        {dataCard ? <CardList data={dataCard} /> : <CardList />}
+        {dataCard ? (
+          <CardList data={dataCard} />
+        ) : (
+          <div className={cls['not-found']}>
+            Cards not found, please fill the form and create card
+          </div>
+        )}
       </div>
     );
   }
