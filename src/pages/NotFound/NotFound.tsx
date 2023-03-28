495d76eeba3d14069fb0bbc34e7c154b5ef827ab
Sergey P <s.pansevi4@gmail.com>
2023-03-28 12:51:17 +0300

    fix: change class components to functional components for About, Form, Home and NotFound pages

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,5 @@
-import { PureComponent } from 'react';
+const NotFound = () => {
+  return <h2>Not Found Page</h2>;
+};
 
-export default class NotFound extends PureComponent {
-  render() {
-    return <h2>Not Found Page</h2>;
-  }
-}
+export default NotFound;
