bdc05becd155d9bc792cffb7b82c4e38e8c87549
Sergey P <s.pansevi4@gmail.com>
2023-03-28 13:15:37 +0300

    fix: change class components to functional components for Layout components

--- src/modules/layout/components/Main/Main.tsx
+++ src/modules/layout/components/Main/Main.tsx
@@ -1,13 +1,12 @@
-import { PureComponent } from 'react';
 import { Outlet } from 'react-router-dom';
 import cls from './Main.module.scss';
 
-export default class Main extends PureComponent {
-  render() {
-    return (
-      <main className={cls.container}>
-        <Outlet />
-      </main>
-    );
-  }
-}
+const Main = () => {
+  return (
+    <main className={cls.container}>
+      <Outlet />
+    </main>
+  );
+};
+
+export default Main;
