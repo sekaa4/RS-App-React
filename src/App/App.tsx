56b2106a1ff43d11d8e8d789002ea84d22c20758
Sergey P <s.pansevi4@gmail.com>
2023-03-28 13:03:34 +0300

    fix: change class components to functional components for App.tsx and WrappedApp.tsx files

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,22 +1,21 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home, About, Form, NotFound } from 'pages';
 import Layout from 'modules/layout';
-import { PureComponent } from 'react';
 import Endpoints from 'models/Endpoints';
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <Routes>
-        <Route path={Endpoints.MAIN} element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path={Endpoints.HOME} element={<Navigate to={Endpoints.MAIN} replace />} />
-          <Route path={Endpoints.ABOUT} element={<About />} />
-          <Route path={Endpoints.FORM} element={<Form />} />
-          <Route path={Endpoints.NOT_FOUND} element={<NotFound />} />
-          <Route path={Endpoints.All} element={<Navigate to={Endpoints.NOT_FOUND} replace />} />
-        </Route>
-      </Routes>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path={Endpoints.MAIN} element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path={Endpoints.HOME} element={<Navigate to={Endpoints.MAIN} replace />} />
+        <Route path={Endpoints.ABOUT} element={<About />} />
+        <Route path={Endpoints.FORM} element={<Form />} />
+        <Route path={Endpoints.NOT_FOUND} element={<NotFound />} />
+        <Route path={Endpoints.All} element={<Navigate to={Endpoints.NOT_FOUND} replace />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
--- src/App/WrappedApp.tsx
+++ src/App/WrappedApp.tsx
@@ -1,13 +1,12 @@
-import { PureComponent } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-export default class WrappedApp extends PureComponent {
-  render() {
-    return (
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-  }
-}
+const WrappedApp = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+export default WrappedApp;
