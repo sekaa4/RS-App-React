0d2bb8b1c212d1d9e46565235e3ec278e15e660d
Sergey P <s.pansevi4@gmail.com>
2023-04-03 11:55:27 +0300

    feat: change CardList, InputSearch and Home components for work with search bar

--- src/modules/card-list/components/CardList/CardList.tsx
+++ src/modules/card-list/components/CardList/CardList.tsx
@@ -1,41 +1,22 @@
 import Data from 'models/Data.type';
-import { useEffect, useState } from 'react';
 import Card from '../Card/Card';
 import cls from './CardList.module.scss';
 
-interface CardListState {
+interface CardListProps {
   data: Data[] | null;
 }
 
-interface CardListProps {
-  data?: Data[];
-}
-
 const CardList = (props: CardListProps) => {
-  const { data: dataProps } = props;
-  const [cardListState, setCardListState] = useState<CardListState>({
-    data: dataProps ?? null,
-  });
-  const { data } = cardListState;
+  const { data } = props;
 
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch(`./Data.json`);
-      const dataFetch = data ?? (await res.json());
-      const newData = dataProps ?? dataFetch;
-      setCardListState((oldState) => ({ ...oldState, data: newData }));
-    };
-    getData();
-  }, [dataProps, data]);
-
-  return cardListState.data ? (
+  return data && data.length !== 0 ? (
     <div className={cls['card-list']}>
-      {cardListState.data.map((dataCharacter: Data) => (
+      {data.map((dataCharacter: Data) => (
         <Card key={dataCharacter.id} data={dataCharacter} />
       ))}
     </div>
   ) : (
-    <div>isLoading...</div>
+    <div>Search not found</div>
   );
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,54 @@
 import CardList from 'modules/card-list/components/CardList/CardList';
 import InputSearch from 'modules/input-search/components/InputSearch/InputSearch';
 import { Button } from 'modules/common';
+import { useState, useEffect, useRef } from 'react';
+import Data from 'models/Data.type';
+import URLConstants from 'models/URLConstants';
 import cls from './Home.module.scss';
 
+interface CardListState {
+  data: Data[] | null;
+}
+
 const Home = () => {
+  const [cardListState, setCardListState] = useState<CardListState>({
+    data: null,
+  });
+  const [searchSubmit, setSearchSubmit] = useState<string>(localStorage.getItem('key') ?? '');
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  const refSearchValue = useRef<string>(localStorage.getItem('key') ?? '');
+
+  const handleClick = () => {
+    setSearchSubmit(refSearchValue.current);
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(`${URLConstants.BASE_URL}?q=${searchSubmit}`);
+        const newData = await res.json();
+        setCardListState((oldState) => ({ ...oldState, data: newData }));
+      } catch (err) {
+        if (err instanceof Error) {
+          throw new Error(err.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [searchSubmit]);
+
   return (
     <div className={cls.home}>
       <h2>Home Page</h2>
       <div className={cls['home__search-bar']}>
-        <InputSearch />
-        <Button text="search" />
+        <InputSearch refSearchValue={refSearchValue} />
+        <Button text="search" handleClick={handleClick} />
       </div>
-      <CardList />
+      {isLoading ? <div>Loading...</div> : <CardList data={cardListState.data} />}
     </div>
   );
 };
