57e7a7e475b87e6d3d46909f21c96ea3d17a39f5
Sergey P <s.pansevi4@gmail.com>
2023-04-27 17:48:20 +0300

    fix: change api test

--- cypress/e2e/api.cy.ts
+++ cypress/e2e/api.cy.ts
@@ -1,11 +1,45 @@
 describe('Check http://localhost:5173 request', () => {
-  it('Get 200 status', () => {
+  it('Get 200 status, home page', () => {
     cy.request({
       method: 'GET',
-      url: `http://localhost:5173`,
+      url: `/`,
     }).then((response) => {
       expect(response.status).to.eq(200);
       expect(response).to.have.property('headers');
+      expect(response.body).to.have.contains('Home Page');
+    });
+  });
+
+  it('Get 200 status, about page', () => {
+    cy.request({
+      method: 'GET',
+      url: `/about`,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response).to.have.property('headers');
+      expect(response.body).to.have.contains('About Page');
+    });
+  });
+
+  it('Get 200 status, form page', () => {
+    cy.request({
+      method: 'GET',
+      url: `/form`,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response).to.have.property('headers');
+      expect(response.body).to.have.contains('Form Page');
+    });
+  });
+
+  it('Get 200 status, Not Found page', () => {
+    cy.request({
+      method: 'GET',
+      url: `/notfound`,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response).to.have.property('headers');
+      expect(response.body).to.have.contains('Not Found Page');
     });
   });
 });
